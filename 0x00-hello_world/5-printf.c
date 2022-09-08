71c11442ce83a2cd0191be6ad93edf592ff702c9
Joseph kakai <josephngalu96@gmail.com>
2022-09-08 22:09:35 +0300

    Corrected task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@
  *
  * Description - using main function
  * prints 'with proper grammar, but the outcome is a piece of art'
- * return '0'
+ * return: 0
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art\n");
 	return(0);
