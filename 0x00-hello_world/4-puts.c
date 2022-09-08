92971c5cf9b8cc9d489efa4949d699da06bbc373
Joseph kakai <josephngalu96@gmail.com>
2022-09-08 23:15:05 +0300

    Corrected task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 # include <stdio.h>
+
 /**
- * main - prints the string in the printf function 
+ *main - prints the string in the printf function 
  *
  * Description: using main function
- * prints \"Programming is like building a multilingual puzzle
+ * prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
-int main(void)
+int main (void)
 {
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
