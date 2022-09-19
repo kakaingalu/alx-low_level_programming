f0197339c7a17c71664c9cc896c4eef8c913e9f6
Joseph kakai <josephngalu96@gmail.com>
2022-09-19 16:41:25 +0300

    corrected number 6

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
-
-void puts2(char *);
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -11,7 +9,7 @@ int main(void)
 {
 	char *str;
 
-	str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\0Holberton";
+	str = "0123456789";
 	puts2(str);
 	return (0);
 }
