a6379f58e506e6c22ed1372ad3470d6cd935ce7c
Joseph kakai <josephngalu96@gmail.com>
2022-10-01 05:52:45 +0300

    Corrected

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,13 +1,7 @@
 #include "main.h"
 
-/**
- * main - Entry point.
- *
- * Return: int.
- */
 int main(void)
 {
-	_puts("\"At the end of the day,my goal was to be the best hacker\"");
-	_puts("\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
