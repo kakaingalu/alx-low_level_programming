449dcbac9c46799a1426f8415157d938e41f4762
Joseph kakai <josephngalu96@gmail.com>
2022-10-04 16:49:31 +0300

    Added task 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,40 @@
+#include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include "main.h"
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to newly allocated space in memory, or NULL if error
  */
-
-
-int _strlen(char *s)
-{
-	int size = 0;
-
-	for (; s[size] != '\0'; size++)
-	return (size);
-}
-
-/**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * Return: pointer
- */
-
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i;
-	char *a;
+	unsigned int i, j, k, l;
+	char *s;
 
 	if (s1 == NULL)
-		s1 = "\0";
-	if (s2 == NULL)
-		s2 = "\0";
-
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-	a = malloc((size1 + size2) * sizeof(char) + 1);
-	if (a == 0)
-		return (0);
-
-	for (i = 0; i <= size1 + size2; i++)
-{
-	if (i < size1)
-		a[i] = s1[i];
+		i = 0;
 	else
-		a[i] = s2[i - size1];
-}
-	m[i] = '\0';
-	return (a);
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	k = i + j + 1;
+	s = malloc(k * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (l = 0; l < i; l++)
+		s[l] = s1[l];
+	for (l = 0; l < j; l++)
+		s[l + i] = s2[l];
+	s[i + j] = '\0';
+	return (s);
 }
