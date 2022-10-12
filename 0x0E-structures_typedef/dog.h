f5b031fdf5dd80293e32f8b0a6eb3004ee8901e7
Josephkakai <josephngalu96@gmail.com>
2022-10-12 16:11:49 +0300

    Corrected Betty errors

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
 struct dog
 {
 	char *name;
