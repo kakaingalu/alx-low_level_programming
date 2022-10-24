a0fae77d45185a827cd038d8c0706b3cab393b94
KAKAIRICHARD <josephngalu96@gmail.com>
2022-10-24 14:50:31 -0700

    Corrected task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	i = 0 ;
+	i = 0;
 
 	while (h)
 	{
@@ -22,7 +22,7 @@ size_t print_list(const list_t *h)
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
-					
+
 		}
 		h = h->next;
 		i++;
