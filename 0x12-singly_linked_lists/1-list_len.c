af1a581e21922d37c3f99b7087aa5eeadc2e99dd
KAKAIRICHARD <josephngalu96@gmail.com>
2022-10-24 15:15:32 -0700

    Addedwq task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a linked list_t list.
+ * @h: the argument to pass in the function.
+ *
+ * Retunr: number of elements in  linked list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num;
+
+	for (num = 0; h; num++)
+	{
+		h = h-> next;
+	}
+	return (num);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,4 +18,5 @@ typedef struct list_s
 } list_t;
 int _putchar(char);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif /*_LIST_H_*/
