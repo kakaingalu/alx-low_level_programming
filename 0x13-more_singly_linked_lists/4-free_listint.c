e7f382c5cf126e5a5a8dd5ea69cacbd7051b83b0
josephkakai <josephngalu96@gmail.com>
2022-10-27 23:51:56 +0300

    Added task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_listint - frees a listint_t list
+* @head: first argument to use.
+*
+* Return: void
+*/
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+
+while (head)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
