4f8e0ea95b5532a6226cd1ce56234a6f3711f5a9
josephkakai <josephngalu96@gmail.com>
2022-10-28 00:02:44 +0300

    Corrected task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a listint_t list
- * @head: first argument to use.
- *
- * Return: void
- */
+* free_listint2 - frees a listint_t list
+* @head: first argument to use.
+*
+* Return: void
+*/
 void free_listint2(listint_t **head)
 {
-    listint_t *temp;
+listint_t *temp;
 
-    while (*head)
-    {
-        temp = *head;
-        *head = (*head)->next;
-        free(temp);
-    }
-    *head = NULL;
+while (*head)
+{
+temp = *head;
+*head = (*head)->next;
+free(temp);
+}
+*head = NULL;
 }
