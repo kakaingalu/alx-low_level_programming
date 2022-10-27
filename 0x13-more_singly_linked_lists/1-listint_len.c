c08bb8937a6d1d77c90802310862e87113c2d8e6
josephkakai <josephngalu96@gmail.com>
2022-10-27 22:46:26 +0300

    corrected betty error

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* listint_len - returns number of elements linked listint_t list.
+* @h: The argument to use.
+*
+* Return: return number of elements.
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t counter;
+
+while (h)
+{
+h = h->next;
+counter++;
+}
+return (counter);
+}
