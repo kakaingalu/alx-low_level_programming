ffe59a907ba3097dd9b80ff691345a94545bfc45
KAKAIRICHARD <josephngalu96@gmail.com>
2022-11-02 15:03:39 -0700

    corrected all betty errors in all tasks

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,9 +6,10 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <unistd.h>
-
+int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
