a413a5c30ec1dbccac9d95cde3c3b9347fe034b5
KAKAIRICHARD <josephngalu96@gmail.com>
2022-11-02 14:33:01 -0700

    corrected all betty errors in all tasks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,9 @@
-#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * check97 - checks for the correct number of arguments
@@ -10,43 +15,42 @@ void check97(int argc)
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 }
 
 /**
  * check98 - checks that file_from exists and can be read
- * @check: checks if true or false
+ * @check: checks if true of false
  * @file: file_from name
- * @fd_from: file descriptor of filr_from, or 1
+ * @fd_from: file descriptor of file_from, or -1
  * @fd_to: file descriptor of file_to, or -1
  *
  * Return: void
  */
-void check98(ssize_t check, char *file,	int fd_from, int fd_to)
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
-		if (fd_from != 1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
 			close(fd_from);
 		if (fd_to != -1)
 			close(fd_to);
 		exit(98);
 	}
-
 }
 
 /**
- * check99 - checks that file_to was create and/or can be written to
+ * check99 - checks that file_to was created and/or can be written to
  * @check: checks if true of false
  * @file: file_to name
- * @fd_from: file descriptor  of file_from, or -1
+ * @fd_from: file descriptor of file_from, or -1
  * @fd_to: file descriptor of file_to, or -1
  *
  * Return: void
  */
-
 void check99(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
@@ -59,7 +63,6 @@ void check99(ssize_t check, char *file, int fd_from, int fd_to)
 		exit(99);
 	}
 }
-
 
 /**
  * check100 - checks that file descriptors were closed properly
@@ -112,4 +115,3 @@ int main(int argc, char *argv[])
 	check100(close_from, fd_from);
 	return (0);
 }
-
