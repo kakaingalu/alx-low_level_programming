cdc8c19c9130208cfff84366403e8fcae9765847
Joseph Kakai <josephngalu96gmail.com>
2022-12-08 19:35:48 +0300

    Corrected betty error

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
  *
- * @: head of the list
+ * @h: head of the list
  * Return: the number of node
  */
 size_t print_dlistint(const dlistint_t *h)
