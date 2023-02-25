821ed12c603a787d81679ff8338437e44f5b3d47
Joseph Kakai <josephngalu96@gmail.com>
2023-02-25 00:56:51 -0800

    Updated

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
+"""A module for working with grid structures.
 """
-Module contais island_perimeter function
-"""
 
 
-def island_perimeter(grid):
-	"""
-	Calculates area of a grid"""
-	if type(grid) is not list:
-		return
-	i = 1
-
-	per = 0
-	while i < len(grid) -1:
-		j = 1
-		while j < len(grid[i])-1:
-			if grid[i][j] == 1:
-				side = 4
-				if grid[i-1][j] == 1:
-					side -= 1
-				if grid[i][j+1] == 1:
-					side -= 1
-				if grid[i+1][j] == 1:
-					side -= 1
-				if grid[i][j-1] == 1:
-					side -= 1
-				per += side
-			j += 1
-		i += 1
-	return(per)
+def island_perimeter(grid: list) -> int:
+    """Computes the perimeter of the island described in the grid.
+    Args:
+        grid (list of list of integers): For each integer, 0 represents
+        water and 1 represents land.
+    Returns:
+        int: The perimeter of the island.
+    """
+    rows = len(grid)
+    perimeter = 0
+    for i in range(rows):
+        cols = len(grid[i])
+        for j in range(cols):
+            if (grid[i][j] == 1):
+                left = 1 if j == 0 or grid[i][j - 1] == 0 else 0
+                top = 1 if i == 0 or grid[i - 1][j] == 0 else 0
+                right = 1 if j == cols - 1 or grid[i][j + 1] == 0 else 0
+                bottom = 1 if i == rows - 1 or grid[i + 1][j] == 0 else 0
+                perimeter += (top + right + bottom + left)
+    return perimeter
